938a648c1e073bfbf138fc554ea98484298f3afb
Sollau <lau.solieri@gmail.com>
2024-08-11 15:05:43 +0200

    Continuing flappyBird

--- myGames/flappyBird.py
+++ myGames/flappyBird.py
@@ -1,39 +1,70 @@
 import pyglet
+import random
 
 # window
-window = pyglet.window.Window(resizable=True)
+window = pyglet.window.Window(1200, 800)
 
 # graphics
 batch = pyglet.graphics.Batch()
 
-# bird
-bird = pyglet.shapes.Circle(500, 500, 30, color = (255, 0, 0,), batch=batch )
-bird.visible = True
+# run distance
+run_dist = 0
+distance = pyglet.text.Label("Distance: " + str(run_dist), 10, 775, batch=batch)
 
-# movement
+
+# bird
+xb = 200
+yb = 400
+delta_x = 50
+delta_y = 25
+bird = pyglet.shapes.Triangle(xb, yb, xb-delta_x, yb+delta_y, xb-delta_x, yb-delta_y, color = (255, 255, 0), batch=batch)
+
+
+# bird movement
 def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.UP:
-        print(f"Key UP pressed")
-        bird.y +=20
+        if bird.y>775:
+            bird.y = 25
+        else:
+            bird.y +=20
         pass
 
     if symbol == pyglet.window.key.DOWN:
-        print(f"Key DOWN pressed")
-        bird.y-=20
+        if bird.y<25:
+            bird.y = 775
+        else:
+            bird.y -=20
         pass
-
+    
+    if symbol == pyglet.window.key.Q:
+        window.close()
+        pass
+        
     if modifiers:
         pass
 
+window.push_handlers(on_key_press)
 
-def on_key_release(symbol, modifiers):
-    if symbol:
-        pass
-    if modifiers:
-        pass
+# obstacles
+obstacles = []
 
+while len(obstacles)<10:
+    y =random.uniform(0, 800)
+    x = random.uniform(xb, 1200)
+    obstacle = pyglet.shapes.Circle(x, y, 20, color = (255, 0, 0,), batch=batch)
+    obstacles.append(obstacle)
 
-window.push_handlers(on_key_press, on_key_release)
+def move_obstacles(dt):
+    for i in range(0, len(obstacles)):
+        obstacles[i].x -= 120*dt
+        if obstacles[i].x < 20:
+            obstacles[i].x = 1180
+            obstacles[i].y = random.uniform(0, 800)
+    global run_dist
+    run_dist+=60*dt
+    distance.text = "Distance: " + str(int(run_dist))
+    
+pyglet.clock.schedule_interval(move_obstacles, 1/60)
 
 
 # draw
