ea1de2e494f5c3bcb382b82a424e6b4e4eecf2ce
Sollau <lau.solieri@gmail.com>
2024-08-13 19:19:48 +0200

    highlight the difference between Cohen and Thjissen methods. Change to a.u., still no good graph

--- thesis/thesis.py
+++ thesis/thesis.py
@@ -2,49 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm  # Importing tqdm for the progress bar
 
-a0 = 0.529  # Bohr radius (in Å)
-a = 5.431 * a0  # Lattice constant of Silicon
+# a0 = 0.52918  # Bohr radius (in Å)
+a = 5.431  # Lattice constant of Silicon (in units of Bohr radius as on Thjissen. For Cohen it's in Å)
 N = 100  # Number of points in the reciprocal space
-tau = np.array([1, 1, 1]) * (a / 8)  # Primitive vector
-h_bar = 6.582e-16  # Reduced Planck constant (in eV*s)
-m = 0.511e6  # Mass of the electron (in eV)
+tau = np.array([1, 1, 1]) * (a / 8)  # Primitive vector (used in Cohen method)
 
-conv = 27.2114 # Conversion factor from Hartree to eV
+# h_bar = 6.582e-16  # Reduced Planck constant (in eV*s)
+# m = 0.511e6  # Mass of the electron (in eV)
+
+# # Primitive vectors of the reciprocal lattice
+# b1 = np.array([-1, 1, 1]) * 2 * np.pi / a
+# b2 = np.array([1, -1, 1]) * 2 * np.pi / a
+# b3 = np.array([-1, 1, -1]) * 2 * np.pi / a
+
+# conv = 27.2114  # Conversion factor from Hartree to eV. For now everything is in atomic units
+
 
 def V_ps(G):
     """Fourier components of the pseudopotential in Eh (Hartree energy units)."""
     if np.isclose(np.dot(G, G), 3 * (2 * np.pi / a) ** 2, atol=1e-5):
-        return (-0.1121*conv)
+        return -0.1121
     elif np.isclose(np.dot(G, G), 8 * (2 * np.pi / a) ** 2, atol=1e-5):
-        return (0.0276*conv)
+        return 0.0276
     elif np.isclose(np.dot(G, G), 11 * (2 * np.pi / a) ** 2, atol=1e-5):
-        return (0.0362*conv)
+        return 0.0362
     else:
         return 0
 
 
 def V_ps_TOT(G):
     """Total pseudopotential, given a vector of the 1st Brillouin zone G."""
-    structure_factor = np.cos(np.dot(G, tau))  # Structure factor
+    structure_factor = np.cos(
+        np.dot(G, tau)
+    )  # Structure factor as in Cohen {it's np.cos(np.pi*(n1+n2+n3)/4)) in Thjissen version, where G = n1*b1+n2*b2+n3*b3}
     V = V_ps(G) * structure_factor  # Total pseudopotential
     return V
 
 
 def T(G, k):
     """Calculates the kinetic energy, given a vector of the 1st Brillouin zone G and a k vector."""
-    T = 0.5 * (np.linalg.norm(k + G)) ** 2  # Kinetic energy (in eV)
-    # aggiungere (h_bar**2/(2*m)) fa appiattire le linee
+    T = (
+        0.5 * (np.linalg.norm(k + G)) ** 2
+    )  # Kinetic energy {factor (h_bar**2/(2*m)) is missing in Thjissen because of atomic uits choice, but it's in Cohen}
     return T
 
 
-n = 2  # Regulates the ranges of G_vectors in H_fill 
+n = 2  # Regulates the ranges of G_vectors in H_fill
 
 
 def H_fill(k):
     """Calculates the Hamiltonian matrix in the 1st Brillouin zone, given k vectors."""
     G_vectors = np.array(
         [[i, j, l] for i in range(-n, n) for j in range(-n, n) for l in range(-n, n)]
-    ) * (4 * np.pi / a)
+    ) * (2 * np.pi / a)
     H = np.zeros((len(G_vectors), len(G_vectors)), dtype=np.float64)
     for i in range(len(G_vectors)):
         H[i][i] += T(G_vectors[i], k)
@@ -76,7 +86,7 @@ if __name__ == "__main__":
     # Energy eigenvalues
     E = []
     for k in tqdm(k_path, desc="Calculating energy eigenvalues"):  # Progress bar
-        e, _ = np.linalg.eigh(H_fill(k))  # Extract eigenvalues e
+        e = np.linalg.eigvalsh(H_fill(k))  # Computes eigenvalues e
         E.append(e)
 
     # Plotting
@@ -91,8 +101,8 @@ if __name__ == "__main__":
         plt.axvline(x=point / len(k_path), color="k", linestyle="--")
     plt.xticks([point / len(k_path) for point in high_symmetry_points], labels)
 
-    plt.xlabel("k-path")
-    plt.ylabel("Energy (eV)")
+    plt.xlabel(r"k-path ($a_0^{-1}$)")
+    plt.ylabel(r"Energy ($E_H$)")
     plt.title("Band Structure of Silicon")
     plt.legend()
     plt.grid(True)
