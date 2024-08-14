5d3ed559ee9b08d837316e9d92af95ae2c9102cd
Sollau <lau.solieri@gmail.com>
2024-08-15 01:29:27 +0200

    Fix to k-path in thesis_C.py

--- thesis/thesis_C.py
+++ thesis/thesis_C.py
@@ -1,43 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm  # Importing tqdm for the progress bar
+import scipy.constants as const
 
 a = 5.431  # Lattice constant of Silicon (in Å)
 N = 100  # Number of points in the reciprocal space
-tau = np.array([1, 1, 1]) * (a / 8)  # Primitive vector
+tau = np.array([1, 1, 1]) * (a / 8)  # Primitive vector of Bravais lattice (in Å)
 
-h_bar = 6.582e-16  # Reduced Planck constant (in eV*s)
-m = 0.511e6  # Mass of the electron (in eV/c**2)
-
-c = 3e8  # Light speed (in m/s)
-
-conv = 27.2114  # Conversion factor from Eh to eV
+J_to_eV = 6.2415e18
+Ry_to_eV = 13.6057039763
 
 
 def V_ps(G):
     """Fourier components (form factors) of the pseudopotential in eV."""
     if np.isclose(np.dot(G, G), 3 * (2 * np.pi / a) ** 2, atol=1e-3):
-        return -0.1121 * conv
+        return (-0.2241*Ry_to_eV)
     elif np.isclose(np.dot(G, G), 8 * (2 * np.pi / a) ** 2, atol=1e-3):
-        return 0.0276 * conv
+        return (0.0551*Ry_to_eV)
     elif np.isclose(np.dot(G, G), 11 * (2 * np.pi / a) ** 2, atol=1e-3):
-        return 0.0362 * conv
+        return (0.0724*Ry_to_eV)
     else:
         return 0
 
 
 def V_ps_TOT(G):
     """Total pseudopotential, given a vector of the 1st Brillouin zone G."""
-    structure_factor = np.cos(np.dot(G, tau))  # Structure factor
-    V = V_ps(G) * structure_factor  # Total pseudopotential
+    S = np.cos(np.dot(G, tau))  # Structure factor (dimensionless)
+    V = V_ps(G) * S  # Total pseudopotential (Ry)
     return V
 
 
 def T(G, k):
     """Calculates the kinetic energy (in eV), given a vector of the 1st Brillouin zone G and a k vector."""
-    T = (
-        ((h_bar*c)**2 / (2 * m *1E-20)) * (np.linalg.norm(k + G)) **2  # Add 1E-20 factor for conversion from Å**2 to m**2
-    )
+    T = (const.hbar**2 / (2 * const.m_e)) * (
+        (1E10 * np.linalg.norm(k + G)) ** 2
+    )*J_to_eV
     return T
 
 
@@ -61,19 +58,18 @@ def H_fill(k):
 if __name__ == "__main__":
     # High-symmetry points (in 1/Å)
     Gamma = np.array([0, 0, 0])
-    X = np.array([2 * np.pi / a, 0, 0])
-    W = np.array([2 * np.pi / a, np.pi / a, 0])
-    K = np.array([3 * np.pi / (2 * a), 3 * np.pi / (2 * a), 0])
-    L = np.array([np.pi / a, np.pi / a, np.pi / a])
+    X = np.array([1, 0, 0]) * (2 * np.pi / a)
+    W = np.array([1, 1 / 2, 0]) * (2 * np.pi / a)
+    K = np.array([3 / 4, 3 / 4, 0]) * (2 * np.pi / a)
+    L = np.array([1 / 2, 1 / 2, 1 / 2]) * (2 * np.pi / a)
 
-    # Irreducible 1st Brillouin zone boundary path
+    # Irreducible 1st Brillouin zone boundary path L g, x, k, g
     k_path = np.concatenate(
         [
-            np.linspace(Gamma, X, 24, endpoint=False),
-            np.linspace(X, W, 12, endpoint=False),
-            np.linspace(W, L, 17, endpoint=False),
-            np.linspace(L, Gamma, 21, endpoint=False),
-            np.linspace(Gamma, K, 26),
+            np.linspace(L, Gamma, 19, endpoint=False),
+            np.linspace(Gamma, X, 22, endpoint=False),
+            np.linspace(X, K, 35, endpoint=False),
+            np.linspace(K, Gamma, 24)
         ]
     )
 
@@ -89,8 +85,8 @@ if __name__ == "__main__":
         plt.plot(np.linspace(0, 1, len(k_path)), band, label=f"Band {i+1}")
 
     # Add vertical lines for high-symmetry points
-    high_symmetry_points = [0, 24, 36, 53, 74, 100]
-    labels = ["Γ", "X", "W", "L", "Γ", "K"]
+    high_symmetry_points = [0, 19, 41, 76, 100]
+    labels = ["L", "Γ", "X", "K",  "Γ"]
     for point in high_symmetry_points:
         plt.axvline(x=point / len(k_path), color="k", linestyle="--")
     plt.xticks([point / len(k_path) for point in high_symmetry_points], labels)
